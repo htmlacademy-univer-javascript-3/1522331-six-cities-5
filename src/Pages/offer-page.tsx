b8c8a2892592a31ddb1e244f76fde7ff8d915ac6
Gurikov Maxim <maximgurikoff@gmail.com>
2024-11-01 20:37:27 +0500

    done review component

--- src/Pages/offer-page.tsx
+++ src/Pages/offer-page.tsx
@@ -2,7 +2,8 @@ import { Helmet } from 'react-helmet-async';
 import { Layout } from '../components/layout.tsx';
 import { OffersList } from '../components/offer/offers-list.tsx';
 import { offerMocks } from '../mocks/offers.ts';
-import { ReviewForm } from '../components/review-form.tsx';
+import { Reviews } from '../components/reviews/reviews.tsx';
+import { reviewMocks } from '../mocks/reviews.ts';
 
 export function OfferPage(): React.JSX.Element {
   return (
@@ -137,44 +138,7 @@ export function OfferPage(): React.JSX.Element {
                   </p>
                 </div>
               </div>
-              <section className="offer__reviews reviews">
-                <h2 className="reviews__title">
-                  Reviews &middot; <span className="reviews__amount">1</span>
-                </h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img
-                          className="reviews__avatar user__avatar"
-                          src="img/avatar-max.jpg"
-                          width="54"
-                          height="54"
-                          alt="Reviews avatar"
-                        />
-                      </div>
-                      <span className="reviews__user-name">Max</span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{ width: '80%' }}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river
-                        by the unique lightness of Amsterdam. The building is
-                        green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">
-                        April 2019
-                      </time>
-                    </div>
-                  </li>
-                </ul>
-                <ReviewForm />
-              </section>
+              <Reviews reviews={reviewMocks} />
             </div>
           </div>
           <section className="offer__map map"></section>
---
+++ src/components/reviews/reviews.tsx
@@ -0,0 +1,16 @@
+﻿import { ReviewForm } from './review-form.tsx';
+import { ReviewsList } from './reviews-list.tsx';
+import { Review } from '../../dataTypes/review.ts';
+
+interface ReviewsProps {
+  reviews: Review[];
+}
+
+export function Reviews({ reviews }: ReviewsProps): React.JSX.Element {
+  return (
+    <section className="offer__reviews reviews">
+      <ReviewsList reviews={reviews} />
+      <ReviewForm />
+    </section>
+  );
+}
