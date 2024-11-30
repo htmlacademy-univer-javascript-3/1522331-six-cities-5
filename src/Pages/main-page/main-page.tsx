9bd7ce360304fbf1ede79cc2c823c6c4882e2abe
Gurikov Maxim <maximgurikoff@gmail.com>
2024-11-30 19:57:48 +0500

    added component

--- src/Pages/main-page/main-page.tsx
+++ src/Pages/main-page/main-page.tsx
@@ -14,6 +14,7 @@ import {
   getCity,
   getSortedOffers,
 } from '../../store/offers/offers.selectors.ts';
+import { EmptyOffersList } from '../../components/offer/empty-offers-list.tsx';
 
 export function MainPage(): React.JSX.Element {
   const [activeOffer, setActiveOffer] = useState<Nullable<Offer>>(null);
@@ -65,22 +66,7 @@ export function MainPage(): React.JSX.Element {
               </div>
             </div>
           ) : (
-            <div className="cities">
-              <div className="cities__places-container cities__places-container--empty container">
-                <section className="cities__no-places">
-                  <div className="cities__status-wrapper tabs__content">
-                    <b className="cities__status">
-                      No places to stay available
-                    </b>
-                    <p className="cities__status-description">
-                      {`We could not find any property available at the moment in
-                        ${city.name}`}
-                    </p>
-                  </div>
-                </section>
-                <div className="cities__right-section no-offers"></div>
-              </div>
-            </div>
+            <EmptyOffersList cityName={city.name} />
           )}
         </main>
       </Layout>
