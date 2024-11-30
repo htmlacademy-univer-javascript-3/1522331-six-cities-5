ef6b35515ae08a0b8ad61639e8f27382afa277c4
Gurikov Maxim <maximgurikoff@gmail.com>
2024-11-30 19:53:21 +0500

    done main-page-empty

--- src/Pages/main-page/main-page.tsx
+++ src/Pages/main-page/main-page.tsx
@@ -19,10 +19,11 @@ export function MainPage(): React.JSX.Element {
   const [activeOffer, setActiveOffer] = useState<Nullable<Offer>>(null);
   const city = useAppSelector(getCity);
   const offers = useAppSelector(getSortedOffers);
-  const offersCountCaption =
-    offers.length === 0
-      ? 'No places to stay available'
-      : `${pluralizeAndCombine('place', offers.length)} to stay in ${city.name}`;
+  const handleActiveOfferChange = useCallback(
+    (offer: Nullable<Offer>) => setActiveOffer(offer),
+    [],
+  );
+  const offersCountCaption = `${pluralizeAndCombine('place', offers.length)} to stay in ${city.name}`;
   return (
     <div className="page page--gray page--main">
       <Layout>
@@ -30,41 +31,57 @@ export function MainPage(): React.JSX.Element {
           <Helmet>6 cities</Helmet>
           <h1 className="visually-hidden">Cities</h1>
           <CitiesList activeCityName={city.name} />
-          <div className="cities">
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{offersCountCaption}</b>
-                <OfferSortSelect />
-                <OffersList
-                  offers={offers}
-                  onActiveOfferChange={useCallback(
-                    (offer: Nullable<Offer>) => setActiveOffer(offer),
-                    [],
-                  )}
-                  isOnMainPage
-                />
-              </section>
-              <div className="cities__right-section">
-                <Map
-                  city={city}
-                  points={offers.map((x) => ({
-                    location: x.location,
-                    id: x.id,
-                  }))}
-                  selectedPoint={
-                    activeOffer
-                      ? {
-                          location: activeOffer?.location,
-                          id: activeOffer?.id,
-                        }
-                      : undefined
-                  }
-                  isOnMainPage
-                />
+          {offers.length > 0 ? (
+            <div className="cities">
+              <div className="cities__places-container container">
+                <section className="cities__places places">
+                  <h2 className="visually-hidden">Places</h2>
+                  <b className="places__found">{offersCountCaption}</b>
+                  <OfferSortSelect />
+                  <OffersList
+                    offers={offers}
+                    onActiveOfferChange={handleActiveOfferChange}
+                    isOnMainPage
+                  />
+                </section>
+                <div className="cities__right-section">
+                  <Map
+                    city={city}
+                    points={offers.map((x) => ({
+                      location: x.location,
+                      id: x.id,
+                    }))}
+                    selectedPoint={
+                      activeOffer
+                        ? {
+                            location: activeOffer?.location,
+                            id: activeOffer?.id,
+                          }
+                        : undefined
+                    }
+                    isOnMainPage
+                  />
+                </div>
               </div>
             </div>
-          </div>
+          ) : (
+            <div className="cities">
+              <div className="cities__places-container cities__places-container--empty container">
+                <section className="cities__no-places">
+                  <div className="cities__status-wrapper tabs__content">
+                    <b className="cities__status">
+                      No places to stay available
+                    </b>
+                    <p className="cities__status-description">
+                      {`We could not find any property available at the moment in
+                        ${city.name}`}
+                    </p>
+                  </div>
+                </section>
+                <div className="cities__right-section no-offers"></div>
+              </div>
+            </div>
+          )}
         </main>
       </Layout>
     </div>
